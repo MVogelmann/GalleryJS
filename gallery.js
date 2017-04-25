b11abb54d93be65947c645df2f3879586e70e8bd
Unknown <mvogelmann@mail.csuchico.edu>
2017-04-25 12:31:08 -0700

    lab completed

--- gallery.js
+++ gallery.js
@@ -1,6 +1,13 @@
-
+var chico = ["UpperBidwell", "LowerBidwell", "WREC", "MadisonBearGarden", "Downtown"]
+var currentIndex = 1000;
 
 function shuffleGallery()
 {
-
+  var arrayLength = chico.length;
+  do {
+    var newIndex = Math.floor(Math.random() * (arrayLength));     //Courtesy of W3schools.com
+  }
+  while (currentIndex == newIndex);
+  currentIndex = newIndex;
+  console.log(chico[newIndex]);
 }
