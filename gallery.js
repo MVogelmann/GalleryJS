c416b251d811cb83fb80c405eece7196d9a115a9
Unknown <mvogelmann@mail.csuchico.edu>
2017-04-27 12:31:38 -0700

    Instead of console output, created a pic gallery

--- gallery.js
+++ gallery.js
@@ -1,4 +1,26 @@
-var chico = ["UpperBidwell", "LowerBidwell", "WREC", "MadisonBearGarden", "Downtown"]
+var chico = [
+  {description: "Upper Bidwell",
+   picture: "http://bidwellpark.org/wp-content/uploads/2014/12/California-Poppies-in-Bidwell-Park.jpg",
+   from: "bidwellpark.org"
+  },
+  {description: "Lower Bidwell",
+   picture: "https://s-media-cache-ak0.pinimg.com/564x/66/ac/32/66ac32e8457e15a0675f219e77599eb5.jpg",
+   from: "pinterest.com"
+  },
+  {description: "WREC",
+   picture: "http://www.sasaki.com/media/files/02sanfranciscoca-state-university-chico-1.jpg",
+   from: "sasaki.com"
+  },
+  {description: "Madison Bear Garden",
+   picture: "https://www.downtownchico.com/webart/listings/headers/x71.jpg.pagespeed.ic.Uoa2c-Foc-.jpg",
+   from: "downtownchico.com"
+  },
+  {description: "Downtown",
+   picture: "http://www.downtownchico.com/webart/downtown-chico.jpg",
+   from: "downtownchico.com"
+  }
+];
+
 var currentIndex = 1000;    //make sure that the variable is not initialized with 0 to length
 
 function shuffleGallery()
@@ -10,5 +32,7 @@ function shuffleGallery()
   }
   while (currentIndex == newIndex);
   currentIndex = newIndex;
-  console.log(chico[newIndex]);
+  document.getElementById("picture").src = chico[newIndex].picture;
+  document.getElementById("caption").innerHTML = chico[newIndex].description;
+  document.getElementById("source").innerHTML = "Courtesy of: " + chico[newIndex].from;
 }
